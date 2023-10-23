3b26d8eaae65b71e3e5748eab724b128f346982a
thayssa.lima <thayssa.mayara@cinbesa.com.br>
2023-10-23 20:56:06 -0300

    feat: Bibliotecário como perfil do sistema

--- desafio-api/src/main/java/com/techlead/desafioapi/service/InserindoUsuarioAdministrador.java
+++ desafio-api/src/main/java/com/techlead/desafioapi/service/InserindoUsuarioAdministrador.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import com.techlead.desafioapi.entity.Usuario;
+import com.techlead.desafioapi.entity.enums.PerfilEnum;
 import com.techlead.desafioapi.repository.UsuarioRepository;
 @Component
 public class InserindoUsuarioAdministrador implements CommandLineRunner{
@@ -19,14 +20,24 @@ public class InserindoUsuarioAdministrador implements CommandLineRunner{
     @Value("${admin.password}")
     private String adminPass;
 
+    @Value("${admin.password}")
+    private String adminBibliotecario;
+
     @Override
     public void run(String...args) throws Exception {
         Optional<Usuario> usuarioOpt = userRepository.findByCpf("36174272001");
         if(!usuarioOpt.isPresent()){
-            Usuario admin = new Usuario("36174272001", "teste@gmail.com", "Admin 1");
+            Usuario admin = new Usuario("36174272001", "administrador@gmail.com", "Admin");
             admin.setSenha(this.passwordEncoder.encode(this.adminPass));
             userRepository.save(admin);
         }
-        
+        Optional<Usuario> usuarioOptBiblio = userRepository.findByCpf("61182391001");
+        if(!usuarioOptBiblio.isPresent()){
+            Usuario admin = new Usuario("61182391001", "bibliotecario@gmail.com", "Bibliotecário");
+            admin.setSenha(this.passwordEncoder.encode(this.adminPass));
+            admin.setPerfil(PerfilEnum.BIBLIOTECARIO);
+            userRepository.save(admin);
+        }
     }
+    
 }
