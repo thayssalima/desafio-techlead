f20cb92cde928e9ac6358c66643dcac2f7b91572
thayssa.lima <thayssa.mayara@cinbesa.com.br>
2023-10-25 17:08:12 -0300

    feat:Colocando as tabelas para o flyway

---
+++ desafio-api/target/classes/db/migration/V1__usuario.sql
@@ -0,0 +1,17 @@
+CREATE TABLE public.usuario (
+	id bigserial NOT NULL,
+	bloqueado bool NULL,
+	cpf varchar(11) NULL,
+	data_bloqueio date NULL,
+	dias_penalidade int4 NULL,
+	email varchar(100) NULL,
+	nome varchar(100) NULL,
+	perfil varchar(255) NULL,
+	senha varchar(100) NULL,
+	CONSTRAINT uk_692bsnqxa8m9fmx7m1yc6hsui UNIQUE (cpf),
+	CONSTRAINT usuario_pkey PRIMARY KEY (id)
+);
+
+
+ALTER TABLE public.usuario OWNER TO postgres;
+GRANT ALL ON TABLE public.usuario TO postgres;
